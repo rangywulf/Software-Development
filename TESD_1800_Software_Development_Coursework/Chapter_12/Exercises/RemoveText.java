257107b9e719f6546151bacc5479e992e833466f
Jess <jxdata@pm.me>
2026-09-11 13:50:41 -0600

    continued working on chapter 12 exercises

---
+++ TESD_1800_Software_Development_Coursework/Chapter_12/Exercises/RemoveText.java
@@ -0,0 +1,18 @@
+public class RemoveText {
+    /** Main Method */
+    public static void main(String[] args) {
+        // Create the file
+            // Set the filename
+            // Create a file object
+            // Write text to the file
+            // Close the file
+        // Get the string to remove
+        // open the file
+        // read the file
+            // read each line
+            // remove all occurrences of the specified string
+            // store the modified line
+        // write the modified contents to the file
+        // close the file
+    }
+}
